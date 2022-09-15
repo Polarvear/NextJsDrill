5fb93d72e276fa9755108edad5e0154bf2539e4e
Polarvear <znzpzp13@naver.com>
2022-09-16 00:01:05 +0900

    first commit

---
+++ pages/photos.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+const photos = () => { // 라우팅이 굉장히 쉽다.
+    return (
+        <div>
+            <h1>My photos</h1>
+        </div>
+    )
+}
+
+export default photos
