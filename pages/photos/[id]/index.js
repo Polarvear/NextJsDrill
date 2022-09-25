4393ffd663c9dc6321014282cfe68f4b0e4497b3
Polarvear <znzpzp13@naver.com>
2022-09-25 23:55:28 +0900

    fallback속성 blocking예시

--- pages/photos/[id]/index.js
+++ pages/photos/[id]/index.js
@@ -5,26 +5,46 @@ import { useRouter } from "next/router"
 
 
 const index = ({photo}) => {
-    const router = useRouter();
-    console.log("router:::", router);
     const {title, url} = photo
+    const router = useRouter();
     return (
         <div>
-            {/* <h2>{title}</h2>
+            <h2>Image{title}</h2>
             <img 
             src={url}
             width={500}
             height={500}
 
             />
+   
     <Link href="/photos">
         <a>go bakc</a>
-    </Link> */}
+    </Link> 
         </div>
     )
 }
 export const getStaticProps = async() => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/photos/1`)
+    const res = await fetch(`https://jsonplaceholder.typicode.com/photos/2`)
+    const photo = await res.json();
+    return {
+        props: {
+            photo
+        }
+    }
 }
 
-export default index
+export const getStaticPaths = async() => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=0&_end=10`)
+    const photos = await res.json();
+    photos.map(photo=> photo.id);
+    const paths = ids.map(id => {
+        return {
+            param: {id: id.toString() }
+        }
+    })
+    return {
+        paths,
+        fallback: false,     //'blocking', //블로킹 속성줬을 때는 지정하지 않은 내용에 대해서도 보여줌
+
+    }
+}
